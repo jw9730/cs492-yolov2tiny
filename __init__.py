7fb65a99c3bcfc9fd3ded7db6d75f51d3d431d1d
Jinwoo Kim <bugkjw@kaist.ac.kr>
2020-04-08 17:28:22 +0900

    Decoding function added

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ import sys
 import numpy as np
 import cv2
 import time
+import math
+import seaborn as sns
 import yolov2tiny
 
 
@@ -40,12 +42,77 @@ def resize_input(im):
     imsz = imsz.transpose((2, 0, 1))
     return imsz
 
-def resize_output(imsz, w0, h0):
-    # imsz: (3, 416, 416) numpy array
-    im = imsz.transpose((1, 2, 0))
-    im = cv2.resize(im, (w0, h0))
-    im = im * 255.
-    return np.asarray(im, dtype=np.uint8)
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+def postprocess(output, w0, h0):
+    """
+    :param output: (1, 125, 13, 13) numpy array
+        Each grid cell corresponds to 125 channels, made up of the 5 bounding boxes predicted by the grid cell
+        and the 25 data elements that describe each bounding box.
+
+    :return: bbox_list: list of tuples (x, y, w, h, text), representing bbox of confidence > 0.25
+        x, y: bbox position in global coordinate, in respect to original image
+        w, h: bbox size in global coordinate, in respect to original image
+        text: box representative text (i.e. semantic category)
+    """
+    output = output.squeeze(0)  # (125, 13, 13)
+    bbox_dim = 25
+
+    # Image pixel distance per one output cell width / height
+    c_x = 32 * (w0 / 416)
+    c_y = 32 * (h0 / 416)
+
+    # Pre-defined anchors: Height and width of the 5 anchors defined by YOLOv2
+    # Source: https://github.com/simo23/tinyYOLOv2
+    # [p_w, p_h] for first bbox, [p_w, p_h] for second bbox, ...
+    # (distance in output cell space)
+    anchors = [1.08, 1.19, 3.42, 4.41, 6.63, 11.38, 9.42, 5.11, 16.62, 10.52]
+
+    bbox_list = list()
+    # Iterate over grid cells
+    for i in range(13):  # y-axis
+        for j in range(13):  # x-axis
+            # 125-dim feature = 5 bbox * 25 bbox feature = 5 bbox * (t_x, t_y, t_w, t_h, t_o, logit(category_0...20))
+            o = output[:, i, j]
+            for bbox_idx in range(5):
+                # Parse prediction prior t_x, t_y, t_w, t_h, t_o, logit[0...19]
+                bbox_feat = o[bbox_dim*bbox_idx:bbox_dim*(bbox_idx+1)]
+                t_x = bbox_feat[0]
+                t_y = bbox_feat[1]
+                t_w = bbox_feat[2]
+                t_h = bbox_feat[3]
+                t_o = bbox_feat[4]
+                logits = bbox_feat[5:]
+
+                # If box confidence >= 0.2, decode x, y, w, h and bbox category
+                p_bbox = sigmoid(t_o)
+                if p_bbox < 0.2:
+                    continue
+
+                # Decode position in image pixel space
+                del_x = sigmoid(t_x) * (w0 / 416)
+                del_y = sigmoid(t_y) * (h0 / 416)
+                x = c_x*j + del_x
+                y = c_y*i + del_y
+
+                # Decode size in image pixel space
+                p_w = anchors[bbox_idx] * 32 * (w0 / 416)
+                p_h = anchors[bbox_idx+1] * 32 * (h0 / 416)
+                w = math.exp(t_w) * p_w
+                h = math.exp(t_h) * p_h
+
+                # Decode category
+                category_idx = np.argmax(logits) + 1
+
+                # Add to decoded bounding box list
+                bbox_list.append((x, y, w, h, category_idx))
+
+    bbox_list = [(50, 40, 100, 80, 0), (100, 50, 70, 90, 1), (100, 100, 70, 90, 10)]
+    return bbox_list
+
 
 def video_object_detection(in_video_path, out_video_path, proc="cpu"):
     # This function runs the inference for each frame and creates the output video.
@@ -64,8 +131,7 @@ def video_object_detection(in_video_path, out_video_path, proc="cpu"):
         print('video_object_detection: Output video not opened')
         sys.exit()
 
-    # TODO: Create an instance of the YOLO_V2_TINY class. Pass the dimension of
-    # the input, a path to weight file, and which device you will use as arguments.
+    # TODO: Create an instance of the YOLO_V2_TINY class. Pass the dimension of the input, a path to weight file, and which device you will use as arguments.
 
     # Start the main loop. For each frame of the video, the loop must do the followings:
     # 1. Do the inference.
@@ -74,8 +140,14 @@ def video_object_detection(in_video_path, out_video_path, proc="cpu"):
     #    them to fit into the original video.
     # 3. Measure the end-to-end time and the time spent only for inference.
     # 4. TODO: Save the intermediate values for the first layer.
-    # TODO: Note that your input must be adjusted to fit into the algorithm,
+    # Note that your input must be adjusted to fit into the algorithm,
     # including resizing the frame and changing the dimension.
+
+    # Color palette for categories
+    color_list = sns.color_palette("husl", 20)
+    for color in color_list:
+        for value in color:
+            value *= 255
 
     # Main loop
     inference_time = 0
@@ -83,36 +155,28 @@ def video_object_detection(in_video_path, out_video_path, proc="cpu"):
         inference_start_time = time.time()
 
         # Get an input frame as a (h0, w0, 3) numpy array
-        ret, in_frame = vcap.read()
+        ret, frame = vcap.read()
 
-        # TODO: Resize input frame to fit YOLOv2_tiny input layer
         # Pre-processing steps: Resize the input image to a (3, 416, 416) array of type float32.
-        frame = resize_input(in_frame)  # (3, 416, 416)
+        frame = resize_input(frame)  # (3, 416, 416)
 
         # TODO: Do the inference.
         # Input: (3, 416, 416) numpy array
         # Output: (1, 125, 13, 13) numpy array
         output = np.ones((1, 125, 13, 13))
 
-        # TODO: Run postprocessing and get an output frame (h0 x w0 x 3 numpy array)
-        # The output is a (125x13x13) tensor where 13x13 is the number of grid cells that the image gets divided into.
-        # Each grid cell corresponds to 125 channels,
-        # made up of the 5 bounding boxes predicted by the grid cell
-        # and the 25 data elements that describe each bounding box (5x25=125).
-        output = output.squeeze(0)
-        # Iterate over grid cells
-        # 125-dim feature = 5 bbox * 25 bbox feature = 5 bbox * (t_x, t_y, t_w, t_h, t_o, logit(category_0...20))
+        # Postprocess
+        bbox_list = postprocess(output, w0, h0)
 
-
-        out_frame = resize_output(frame, w0, h0)
-
-        # TODO: Adjust bbox x, y, w, h and write on resized output frame
-        x, y, w, h = (20, 40, 60, 80)
-        cv2.rectangle(out_frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
+        # Layout on
+        for x, y, w, h, category_idx in bbox_list:
+            assert (0 < x < w0) and (0 < y < h0) and (0 < w < w0) and (0 < h < h0)
+            # x, y: box center
+            cv2.rectangle(frame, (x-w/2, y-h/2), (x+w/2, y+h/2), color_list[category_idx], 3)
 
         inference_time += (time.time() - inference_start_time)
         # Accumulate final output frame to VideoWriter object
-        out.write(out_frame)
+        out.write(frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print("Main loop terminated after processing %d frames, %d expected" % (t+1, n_frames))
