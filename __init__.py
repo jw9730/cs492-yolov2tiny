e07a2f7e95bd4698f5d5719807e4accd43603146
Jinwoo Kim <bugkjw@kaist.ac.kr>
2020-04-21 04:14:30 +0900

    debugging graph forward pass

--- __init__.py
+++ __init__.py
@@ -106,11 +106,11 @@ def video_object_detection(in_video_path, out_video_path, proc="cpu"):
 
             cv2.rectangle(frame, lefttop, rightbottom, color, 1)
             text = best_class_name
-            (text_width, text_height) = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, thickness=1.5)[0]
-            box_coords = ((lefttop[0], rightbottom[1]), (lefttop[0] + text_width + 2, rightbottom[1] - text_height - 2))
+            (text_width, text_height) = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=1)[0]
+            box_coords = ((lefttop[0], rightbottom[1]), (lefttop[0] + text_width + 2, rightbottom[1] + text_height + 2))
             cv2.rectangle(frame, box_coords[0], box_coords[1], color, cv2.FILLED)
-            cv2.putText(frame, text, (lefttop[0], rightbottom[1]), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5,
-                        color=(255, 255, 255), thickness=1.5)
+            cv2.putText(frame, text, (lefttop[0], rightbottom[1]), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=(255, 255, 255), thickness=1)
 
         # Accumulate final output frame to VideoWriter object
         out.write(frame)
