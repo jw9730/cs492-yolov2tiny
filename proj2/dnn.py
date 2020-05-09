ada8855a3ba76ab0ac4be0987a70a6fea0eeab85
Jinwoo Kim <bugkjw@kaist.ac.kr>
2020-05-09 21:09:24 +0900

    maxpool error fix

--- proj2/dnn.py
+++ proj2/dnn.py
@@ -413,7 +413,7 @@ class MaxPool2D(DnnNode):
                 res = np.amax(input_rf.reshape((out_b, k_h * k_w, out_c)), axis=1)
 
                 if int(x == 0) + int(y == 0) + int(y == out_h - 1) + int(x == out_w - 1) >= 2:
-                    print(input_rf[:, :, :, 0:2])
+                    print(input_rf[:, :, 0:2])
                 if not np.isfinite(res).all():
                     print(res)
                     raise RuntimeError
