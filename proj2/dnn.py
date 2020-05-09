e7115c6653ea946052da67f43b795f106319de23
Jinwoo Kim <bugkjw@kaist.ac.kr>
2020-05-09 16:45:06 +0900

    zero padding

--- proj2/dnn.py
+++ proj2/dnn.py
@@ -240,8 +240,11 @@ class Conv2D(DnnNode):
 
                         # (n, y, x, m) for batch, row, column and channel of output feature map
                         # convolve with kernel using 1d dot product
-                        kernel_1d = self.kernel.flatten()
-                        input_1d = padded_input[n*s_b, y*s_h:y * s_h + k_h, x*s_w:x*s_w + k_w, m*s_c].flatten()
+                        kernel_1d = self.kernel[:, :, :, m].flatten()
+                        input_1d = padded_input[(n * s_b),
+                                   (y * s_h):(y * s_h + k_h),
+                                   (x * s_w):(x * s_w + k_w),
+                                   :].flatten()
                         dot_product = np.dot(kernel_1d, input_1d)
 
                         # Loop over kernel pixels
