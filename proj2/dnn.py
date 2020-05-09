913afd1ee53eb7de417b2efafa982bc2cd05f54e
Jinwoo Kim <bugkjw@kaist.ac.kr>
2020-05-09 12:54:44 +0900

    zero padding

--- proj2/dnn.py
+++ proj2/dnn.py
@@ -232,6 +232,10 @@ class Conv2D(DnnNode):
             for m in range(out_c):
                 for y in range(out_h):
                     for x in range(out_w):
+                        # test for boundary
+                        assert (y != out_h - 1) or (y * s_h + k_h == padded_input.shape[1] - 1)
+                        assert (x != out_w - 1) or (x * s_w + k_w == padded_input.shape[2] - 1)
+
                         # (n, y, x, m) for batch, row, column and channel of output feature map
                         # Loop over kernel pixels
                         for c in range(k_in):
@@ -241,9 +245,6 @@ class Conv2D(DnnNode):
                                     # corresponding pos on input: (n * s_b, y * s_h, x * s_w, c * s_c) + (0, i, j, 0)
                                     self.result[n, y, x, m] += self.kernel[i, j, c, m] * \
                                                                padded_input[n * s_b, y * s_h + i, x * s_w + j, c * s_c]
-                        # test for boundary
-                        assert y != out_h - 1 or y * s_h + k_h == padded_input.shape[1] - 1
-                        assert x != out_w - 1 or x * s_w + k_w == padded_input.shape[2] - 1
         print("Conv2D: elapsed time %.2fsec" % (time.time() - mark))
         return self.result
 
@@ -384,13 +385,14 @@ class MaxPool2D(DnnNode):
             for m in range(out_c):
                 for y in range(out_h):
                     for x in range(out_w):
+                        # test for boundary
+                        assert (y != out_h - 1) or (y * s_h + k_h == padded_input.shape[1] - 1)
+                        assert (x != out_w - 1) or (x * s_w + k_w == padded_input.shape[2] - 1)
+
                         self.result[n, y, x, m] = np.amax(padded_input[(n * s_b):(n * s_b + k_b),
                                                           (y * s_h):(y * s_h + k_h),
                                                           (x * s_w):(x * s_w + k_w),
                                                           (m * s_c):(m * s_c + k_c)])
-                        # test for boundary
-                        assert y != out_h - 1 or y * s_h + k_h == padded_input.shape[1] - 1
-                        assert x != out_w - 1 or x * s_w + k_w == padded_input.shape[2] - 1
 
         print("MaxPool2D: elapsed time %.2f" % (time.time() - mark))
         return self.result
