ef0b26a41368ad6aa318c479d85a5da99cca5419
Jinwoo Kim <bugkjw@kaist.ac.kr>
2020-05-20 17:19:55 +0900

    experiment

--- proj3/src/dnn_openblas.c
+++ proj3/src/dnn_openblas.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cblas.h>
+#include <assert.h>
 
 void dot_product(float *v1, float *v2, float *res, int n) {
-    // n: length of vectors
+    // n: length of v1 and v2
     cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
                 1, 1, n,
                 1.0, v1, n, v2, 1,
@@ -11,12 +12,11 @@ void dot_product(float *v1, float *v2, float *res, int n) {
 }
 
 void ki_apply(float *K, float *I, float *res, int in_size, int out_size) {
+    assert(!K && !I && !res);
+
     // K: (in_size, out_size)
     // I: (1, in_size)
     // res: (1, out_size)
-
-    ASSERT (!K && !I && !res);
-
     cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
                 1, out_size, in_size,
                 1.0, I, in_size, K, out_size,
--- proj3/src/dnn_openblas.py
+++ proj3/src/dnn_openblas.py
@@ -181,6 +181,9 @@ class Conv2D(DnnNode):
         toc = time.time()
         print("Conv2D: baseline elapsed time {}s".format(toc - tic))
 
+        assert np.count_nonzero(np.isnan(self.result)) == 0, "Conv2D: {} nans found in output array of baseline method"\
+            .format(np.count_nonzero(np.isnan(self.result)))
+
         # offloaded
         tic = time.time()
         # float pointer type: every n-d array will be modified to 1d float array
@@ -222,13 +225,10 @@ class Conv2D(DnnNode):
                 # accumulate pixel output
                 full_result[0, ow, oh, :] = np.ctypeslib.as_array(res, (self.OC,))
 
-        assert np.count_nonzero(np.isnan(full_result)) == 0, "Conv2D: {} nans found in output array".format(np.count_nonzero(np.isnan(full_result)))
-
         toc = time.time()
         print("Conv2D: offloaded elapsed time {}s".format(toc - tic))
 
-        # free memory
-        del pin, k_1d
+        assert np.count_nonzero(np.isnan(full_result)) == 0, "Conv2D: {} nans found in output array".format(np.count_nonzero(np.isnan(full_result)))
         assert (full_result - self.result).mean() < 1e-5, "Conv2D: consistency check failed with error {}".format((full_result - self.result).mean())
         self.result = full_result
 
