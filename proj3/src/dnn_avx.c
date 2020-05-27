f3a1a3629b14310c2e8ed0a16d1ef0a869cbd726
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-05-27 20:15:36 +0900

    bug fix

--- proj3/src/dnn_avx.c
+++ proj3/src/dnn_avx.c
@@ -17,7 +17,6 @@ struct args {
 };
 
 __m256 get_chunk(float * v, int n){
-
 #ifdef DEBUG
     printf("get_chunk: v [");
     for (int i=0; i<n; i++) printf("%3.2f ", v[i]);
@@ -91,24 +90,24 @@ void ki_apply(float *K, float *I, float *R, int in_size, int out_size) {
             args = malloc(sizeof (struct args));
 
             // convert subarrays into 256-bit chunks
-            n_f = MIN(in_size - 8 * j, 8);
+            //n_f = MIN(in_size - 8 * j, 8);
 #ifdef DEBUG
             printf("\nki_apply: chunk idx [%d]/[%d], # elements %d, args @ %p\n", j, n_c-1, n_f, args);
 #endif
-            args->x = get_chunk(K_o + 8 * j, n_f);
-            args->y = get_chunk(I + 8 * j, n_f);
+            //args->x = get_chunk(K_o + 8 * j, n_f);
+            //args->y = get_chunk(I + 8 * j, n_f);
             args->o = R_o;
 #ifdef DEBUG
             printf("ki_apply: create thread %d\n", i * n_c + j);
 #endif
             // run thread
-            pthread_create(tid + (i * n_c + j), NULL, func, (void *)(args));
+            //pthread_create(tid + (i * n_c + j), NULL, func, (void *)(args));
         }
     }
 
     for (int i=0; i<out_size; i++){
         for (int j=0; j<n_c; j++){
-            pthread_join(tid[i * n_c + j], NULL);
+            //pthread_join(tid[i * n_c + j], NULL);
             printf("thread %d ends\n", i * n_c + j);
         }
     }
