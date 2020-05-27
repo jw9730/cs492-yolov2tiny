f214d2b42042b8bc830a8c99467ff41650d007ef
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-05-27 18:21:51 +0900

    bug fix

--- proj3/src/dnn_avx.c
+++ proj3/src/dnn_avx.c
@@ -65,7 +65,7 @@ void ki_apply(float *K, float *I, float *R, int in_size, int out_size) {
             args->o = R_o;
 
             // run thread
-            pthread_create(tid + (i * n_c + j), NULL, func, (void *)(args));
+            pthread_create(tid + (i * n_c + j), NULL, &func, (void *)(args));
         }
     }
 
