2044de6f3aa848e44220a8c44e20b3ace517c106
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-03 11:11:23 +0900

    compiler lines added, CUDA will fail

--- proj3/src/dnn_cublas.c
+++ proj3/src/dnn_cublas.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <cuda_runtime.h>
 #include "cublas_v2.h"
 
 int ki_apply(float *K, float *I, float *res, int in_size, int out_size) {
-    assert(K != NULL);
-    assert(I != NULL);
-    assert(res != NULL);
-
     // K: (in_size, out_size)
     // I: (1, in_size)
     // res: (1, out_size)
