34f03eeb46b25d93388960d07e8f645dbff508a8
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-03 15:33:07 +0900

    cuda debug

--- proj3/src/dnn_cuda.py
+++ proj3/src/dnn_cuda.py
@@ -202,7 +202,7 @@ class Conv2D(DnnNode):
         cuda_result = np.ctypeslib.as_array(out_p, (1, self.OW, self.OH, self.OC))
         toc = time.time()
         print("Conv2D: TOEPLITZ-CUDA elapsed time {:1.5f}s".format(toc - tic))
-        print("Conv2D: error {:1.5f}".format(abs(cuda_result - ref_result).mean())
+        print("Conv2D: error {:1.5f}".format(abs(cuda_result - ref_result).mean()))
         assert abs(cuda_result - ref_result).mean() < 1e-5, "Conv2D: correctness check failed with mean err {}".format(abs(cuda_result - ref_result).mean())
         self.result = cuda_result
 
