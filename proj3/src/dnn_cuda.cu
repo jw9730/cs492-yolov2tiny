25aab5a812a3bb8bf86dd77cc54c6d4c38ea595f
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-04 00:28:17 +0900

    input stationary CUDA

--- proj3/src/dnn_cuda.cu
+++ proj3/src/dnn_cuda.cu
@@ -107,7 +107,7 @@ __global__ void conv_ws(float *I, float *K, float *R, int iw, int ih, int ow, in
     int pos = ofs + tid;
     int w = pos/oc/oh;
     int h = pos/oc%oh;
-    printf("[%d, %d, %d], bid %d, tid %d/%d, BLOCKS_PER_CHANNEL %d\n", w, h, cid, tid, bid, n_tid-1, BLOCKS_PER_CHANNEL);
+    printf("[%d, %d, %d], bid %d, tid %d/%d, BLOCKS_PER_CHANNEL %d\n", w, h, cid, bid, tid, n_tid-1, BLOCKS_PER_CHANNEL);
     float *o = R + INDEX_ROW_MAJOR_3(w,h,cid, ow,oh,oc);
     for (int i=0; i<kw; i++){
         for (int j=0; j<kh; j++){
