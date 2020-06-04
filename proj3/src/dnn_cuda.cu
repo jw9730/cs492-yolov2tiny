ead709bf6a00a926e2562d2ccd2b783733f408e1
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-05 01:51:01 +0900

    all cleanup, added debug flag for layerwise running time analysis

--- proj3/src/dnn_cuda.cu
+++ proj3/src/dnn_cuda.cu
@@ -31,11 +31,6 @@ __global__ void conv_ws(float *I, float *K, float *R, int iw, int ih, int ow, in
     int tid = threadIdx.x;
     int pid = bid % BLOCKS_PER_CHANNEL; // pixel block index (within channel)
     int cid = bid / BLOCKS_PER_CHANNEL; // output channel index
-    // compute block index in output pixel dimension
-    int ofs = pid * THREADS_PER_BLOCK;
-    // handle boundary
-    if (tid >= ((ow * oh - ofs < THREADS_PER_BLOCK)? (ow * oh - ofs) : THREADS_PER_BLOCK)) return;
-
     // declare on-chip shared memory
     extern __shared__ float M[];
     // read input data once per block (shared across threads)
@@ -44,33 +39,45 @@ __global__ void conv_ws(float *I, float *K, float *R, int iw, int ih, int ow, in
     int f = kw*kh*ic;
     int load_per_thread = ceil(float(f)/float(THREADS_PER_BLOCK));
     int l = load_per_thread * tid;
+    int u = load_per_thread * (tid + 1);
     if (l < f) {
-        int u = load_per_thread * (tid + 1);
-        int lim = (u<f)?u:f;
-        for (int idx=l; idx<lim; idx++){
-            int i = idx/ic/kh;
-            int j = idx/ic%kh;
+        u = (u < f)? u : f;
+        for (int idx=l; idx<u; idx++){
             int k = idx%ic;
+            int j = idx/ic%kh;
+            int i = idx/ic/kh;
             M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] = K[INDEX_ROW_MAJOR_4(i,j,k,cid, kw,kh,ic,oc)];
         }
     }
+    /*
+    if(tid == 0){
+        for (int i=0; i<kw; i++){
+            for (int j=0; j<kh; j++){
+                for (int k=0; k<ic; k++){
+                    M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] = K[INDEX_ROW_MAJOR_4(i,j,k,cid, kw,kh,ic,oc)];
+                }
+            }
+        }
+    }
+    */
     // wait until data is ready
     __syncthreads();
+    // compute block index in output pixel dimension
+    int ofs = pid * THREADS_PER_BLOCK;
+    // handle boundary
+    if (tid >= ((ow * oh - ofs < THREADS_PER_BLOCK)? (ow * oh - ofs) : THREADS_PER_BLOCK)) return;
     // retrieve output pixel
-    int w = (ofs+tid)/oh;
-    int h = (ofs+tid)%oh;
-    int w_ofs = w*sw;
-    int h_ofs = h*sh;
-    float acc = 0;
+    int w_ofs = (ofs+tid)/oh*sw;
+    int h_ofs = (ofs+tid)%oh*sh;
+    float * o = R + INDEX_ROW_MAJOR_3(w,h,cid, ow,oh,oc);
     // apply convolution
     for (int i=0; i<kw; i++){
         for (int j=0; j<kh; j++){
             for (int k=0; k<ic; k++){
-                acc += I[INDEX_ROW_MAJOR_3(w_ofs+i,h_ofs+j,k, iw,ih,ic)] * M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)];
+                atomicAdd(o, I[INDEX_ROW_MAJOR_3(w_ofs+i,h_ofs+j,k, iw,ih,ic)] * M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)]);
             }
         }
     }
-    R[INDEX_ROW_MAJOR_3(w,h,cid, ow,oh,oc)] = acc;
 }
 __global__ void conv_is(float *I, float *K, float *R, int iw, int ih, int ow, int oh, int kw, int kh, int sw, int sh, int ic, int oc){
     // input stationary
@@ -82,13 +89,6 @@ __global__ void conv_is(float *I, float *K, float *R, int iw, int ih, int ow, in
     // compute output pixel of the block
     int h = pid % oh;
     int w = pid / oh;
-    int w_ofs = w*sw;
-    int h_ofs = h*sh;
-    // compute block index in output channel dimension
-    int ofs = cid * THREADS_PER_BLOCK;
-    // handle boundary
-    if (tid >= ((oc - ofs < THREADS_PER_BLOCK)? (oc - ofs) : THREADS_PER_BLOCK)) return;
-
     // declare on-chip shared memory
     extern __shared__ float M[];
     // read input data once per block (shared across threads)
@@ -97,28 +97,42 @@ __global__ void conv_is(float *I, float *K, float *R, int iw, int ih, int ow, in
     int f = kw*kh*ic;
     int load_per_thread = ceil(float(f)/float(THREADS_PER_BLOCK));
     int l = load_per_thread * tid;
+    int u = load_per_thread * (tid + 1);
     if (l < f) {
-        int u = load_per_thread * (tid + 1);
-        int lim = (u<f)?u:f;
-        for (int idx=l; idx<lim; idx++){
-            int i = idx/ic/kh;
-            int j = idx/ic%kh;
+        u = (u < f)? u : f;
+        for (int idx=l; idx<u; idx++){
             int k = idx%ic;
-            M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] = I[INDEX_ROW_MAJOR_3(w_ofs+i,h_ofs+j,k, iw,ih,ic)];
+            int j = idx/ic%kh;
+            int i = idx/ic/kh;
+            M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] = I[INDEX_ROW_MAJOR_3(w*sw+i,h*sh+j,k, iw,ih,ic)];
         }
     }
-    // wait until data is ready
-    __syncthreads();
-    // apply convolution
-    float acc = 0;
-    for (int i=0; i<kw; i++){
-        for (int j=0; j<kh; j++){
-            for (int k=0; k<ic; k++){
-                acc += M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] * K[INDEX_ROW_MAJOR_4(i,j,k,ofs+tid, kw,kh,ic,oc)];
+    /*
+    if(tid == 0){
+        for (int i=0; i<kw; i++){
+            for (int j=0; j<kh; j++){
+                for (int k=0; k<ic; k++){
+                    M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] = I[INDEX_ROW_MAJOR_3(w*sw+i,h*sh+j,k, iw,ih,ic)];
+                }
             }
         }
     }
-    R[INDEX_ROW_MAJOR_3(w,h,ofs+tid, ow,oh,oc)] = acc;
+    */
+    // wait until data is ready
+    __syncthreads();
+    // compute block index in output channel dimension
+    int ofs = cid * THREADS_PER_BLOCK;
+    // handle boundary
+    if (tid >= ((oc - ofs < THREADS_PER_BLOCK)? (oc - ofs) : THREADS_PER_BLOCK)) return;
+    // apply convolution
+    float *o = R + INDEX_ROW_MAJOR_3(w,h,ofs+tid, ow,oh,oc);
+    for (int i=0; i<kw; i++){
+        for (int j=0; j<kh; j++){
+            for (int k=0; k<ic; k++){
+                atomicAdd(o, M[INDEX_ROW_MAJOR_3(i,j,k, kw,kh,ic)] * K[INDEX_ROW_MAJOR_4(i,j,k,ofs+tid, kw,kh,ic,oc)]);
+            }
+        }
+    }
 }
 extern "C"
 void conv2d(float * I, float * K, float * R, int iw, int ih, int ow, int oh, int kw, int kh, int sw, int sh, int ic, int oc) {
@@ -130,38 +144,33 @@ void conv2d(float * I, float * K, float * R, int iw, int ih, int ow, int oh, int
     // loop over outer dimensions, and compute dot product in chunks of size 512
     // kernel function: convolution for a single sliding window
     // allocate the memory on the GPU
-    cudaMalloc( (void**)&dev_I, iw * ih * ic * sizeof(float) );
-    cudaMalloc( (void**)&dev_K, kw * kh * ic * oc * sizeof(float) );
-    cudaMalloc( (void**)&dev_R, ow * oh * oc * sizeof(float) );
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_I, iw * ih * ic * sizeof(float) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_K, kw * kh * ic * oc * sizeof(float) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_R, ow * oh * oc * sizeof(float) ) );
     // copy the arrays to the GPU
-    cudaMemcpy( dev_I, I, iw * ih * ic * sizeof(float), cudaMemcpyHostToDevice );
-    cudaMemcpy( dev_K, K, kw * kh * ic * oc * sizeof(float), cudaMemcpyHostToDevice );
+    HANDLE_ERROR( cudaMemcpy( dev_I, I, iw * ih * ic * sizeof(float), cudaMemcpyHostToDevice ) );
+    HANDLE_ERROR( cudaMemcpy( dev_K, K, kw * kh * ic * oc * sizeof(float), cudaMemcpyHostToDevice ) );
     // how to organize blocks?
     // maximizing data reuse and parallelism within a block
     // dynamic on-chip memory allocation
     int BLOCK_MEMSIZE = kw * kh * ic * sizeof(float);
-    if (ow*oh > 10 * THREADS_PER_BLOCK){
-#ifdef DEBUG
-        printf("dnn_cuda.so: weight stationary\n");
-#endif
+    if (ow*oh > THREADS_PER_BLOCK){
         // weight stationary
         // within a block, hold kernel and thread over output pixels
         int BLOCKS_PER_CHANNEL = ceil(float(ow*oh)/float(THREADS_PER_BLOCK));
         conv_ws<<<oc*BLOCKS_PER_CHANNEL,THREADS_PER_BLOCK,BLOCK_MEMSIZE>>>(dev_I, dev_K, dev_R, iw, ih, ow, oh, kw, kh, sw, sh, ic, oc);
     }else{
-#ifdef DEBUG
-        printf("dnn_cuda.so: input stationary\n");
-#endif
         // input stationary
         // within a block, hold input and thread over output channels
         int BLOCKS_PER_PIXEL = ceil(float(oc)/float(THREADS_PER_BLOCK));
         conv_is<<<ow*oh*BLOCKS_PER_PIXEL,THREADS_PER_BLOCK,BLOCK_MEMSIZE>>>(dev_I, dev_K, dev_R, iw, ih, ow, oh, kw, kh, sw, sh, ic, oc);
     }
     // copy the array back from the GPU to the CPU
-    cudaMemcpy( R, dev_R, ow * oh * oc * sizeof(float), cudaMemcpyDeviceToHost );
+    HANDLE_ERROR( cudaMemcpy( R, dev_R, ow * oh * oc * sizeof(float), cudaMemcpyDeviceToHost ) );
     // cleanup
     cudaFree(dev_I); cudaFree(dev_K); cudaFree(dev_R);
 }
+
 
 
 
